d8b89b7fdf82edeb4ffacc6623014061bd8df4ca
Sam Curran <samuel.curran@gmail.com>
2018-04-22 16:02:34 -0400

    switched unit tests to integers to avoid floating-point precision issues

--- dimensionalnumber/test_dimnum.py
+++ dimensionalnumber/test_dimnum.py
@@ -1,29 +1,20 @@
 import dimnum
 
-from hypothesis.strategies import floats
+from hypothesis.strategies import integers
 from hypothesis import assume, given
 
 
-def test_construction(scalar, units):
-    pass
-
-@given(
-    floats(allow_nan=False, allow_infinity=False),
-    floats(allow_nan=False, allow_infinity=False),
-    floats(allow_nan=False, allow_infinity=False))
+@given(integers(), integers(), integers())
 def test_addition(a, b, c):
     assert (a + b) == (b + a)
     assert ((a + b) + c) == (a + (b + c))
-    assert (a + 0.0) == a
+    assert (a + 0) == a
     assert (a * (b + c)) == ((a * b) + (a * c))
 
 
-@given(
-    floats(allow_nan=False, allow_infinity=False),
-    floats(allow_nan=False, allow_infinity=False),
-    floats(allow_nan=False, allow_infinity=False))
+@given(integers(), integers(), integers())
 def test_multiplication(a, b, c):
     assert (a * b) == (b * a)
     assert ((a * b) * c) == (a * (b * c))
-    assert (a * 1.0) == a
+    assert (a * 1) == a
     assert (a * (b + c)) == ((a * b) + (a * c))
